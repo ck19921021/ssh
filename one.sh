36e0abd6aced3875878a6c6d41878c2cc96a4a91
ck19921021 <2464017733@qq.com>
2020-10-12 14:48:06 +0800

    Update one.sh

--- one.sh
+++ one.sh
@@ -1,2 +1,3 @@
 STREAM="stable"
-coreos-installer download -s "${STREAM}" -p qemu -f qcow2.xz --decompress -C ~/.local/share/libvirt/images/
+mkdir ova-templates
+coreos-installer download -s "${STREAM}" -p vmware -f ova -C ./ova-templates/
