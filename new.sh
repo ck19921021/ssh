70bf74ce1ad4f3b1e348a8c54ae04ce8472ab233
ck19921021 <2464017733@qq.com>
2020-10-15 10:09:04 +0800

    Update new.sh

--- new.sh
+++ new.sh
@@ -1,2 +1,2 @@
-podman run --privileged --pull=always --rm -v .:/data -w /data \
-    quay.io/coreos/coreos-installer:release download -f iso
+sudo coreos-installer install /dev/sda \
+    --ignition-url https://example.com/example.ign
